5dfbe2d35005cdf17d6f0418e61ade752ae4af29
Farah Student <farah.gus@hotmail.com>
2019-02-20 12:44:30 -0500

    finished max and min function

--- array-drills.js
+++ array-drills.js
@@ -1,19 +1,35 @@
 'use strict';
 
 function max(numbers){
+  if (numbers.length === 0){
+    return undefined;
+  }
+
   let i = 0;
   let currentMax = 0;
   while (i < numbers.length) {
     if (currentMax < numbers[i]) {
       currentMax = numbers[i];
     }
+    i++;
   }
   return currentMax;
 }
 
-function min (numbers){
-  if (numbers.length == 0){
+function min(numbers){
+  if (numbers.length === 0){
     return undefined;
   }
+  let i = 0;
+  let currentMin = 0;
 
+  while (i < numbers.length){
+    if (currentMin > numbers[i]){
+      currentMin = numbers[i];
+    }
+    i++;
+  }
+  return currentMin;
 }
+
+
