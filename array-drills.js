d458613546e1081502d4571c2d8958ece8e777b6
Farah Student <farah.gus@hotmail.com>
2019-02-20 14:38:25 -0500

    added rerepeat function

--- array-drills.js
+++ array-drills.js
@@ -39,3 +39,16 @@ function average(numbers) {
 }
 
 
+function repeat(fn, n){
+  for (let i = 0; i < n; i++){
+    fn();
+  }
+}
+
+function hello(){
+  console.log('Hello world');
+}
+
+function goodbye(){
+  console.log('Goodbye world');
+}
